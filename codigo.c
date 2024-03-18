383165f77bccf6334cdd04cc4aa7365c7337f613
Jadson-Hipolito <142536589+Jadson-Hipolito@users.noreply.github.com>
2024-03-18 19:29:20 -0300

    0.2

--- codigo.c
+++ codigo.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 int menu = 0;
 int main(){
- printf("\nSIG-Inventory - Sistema de Gerenciamento de inventario\n");
- printf("\nTem duas telas abaixo escolhendo 1 ou 2\n\n");
+ printf("\n=====================================\n");
+ printf("=== Menu Inventario ===\n");
+ printf("=====================================\n");
+ printf("1 - Produtos\n");
+ printf("2 - Funcionarios\n");
+ printf("3 - Entrada e Saida\n");
+ printf("0 - Finalizar Programa\n");
+ printf("=====================================\n");
+ printf("Escolha uma opção: ");
  scanf("%d", &menu);
  switch (menu) {
    case 1:
-     printf("\n███████████████████████████████████████████████████████████████████"
-          "█████████\n");
-     printf("\nTela 1 ainda sem função\n")   ;
-     printf("\n███████████████████████████████████████████████████████████████████"
-          "█████████\n");
-     break;
-   case 2:
-     printf("\n███████████████████████████████████████████████████████████████████"
-          "█████████\n");
-     printf("\nTela 2 ainda sem função\n");
-     printf("\n███████████████████████████████████████████████████████████████████"
-          "█████████\n");
-     break;
- }
+    printf("\n=====================================\n");
+    printf("=== Menu Produtos ===\n");
+    printf("=====================================\n");
+    printf("1 - Cadastra Produto Novo\n");
+    printf("2 - Excluir Produto\n");
+    printf("3 - Mudar Quantidade do Produto\n");
+    printf("0 - Finalizar Programa\n");
+    printf("=====================================\n");
+    printf("Escolha uma opção: ");
+    break;
+}
 }
